2a5738890e79c1f7f9200387e070ca77a7eb9030
Neil Dahlke <neil@dahlke.io>
2018-12-01 16:09:55 -0800

    Precompile go binary and insert it into the Docker image.

--- eklhad/eklhad_entry.sh
+++ eklhad/eklhad_entry.sh
@@ -4,9 +4,7 @@ set -e
 if [[ "$1" = "eklhad" ]]; then
     echo "Starting server"
     cd /eklhad/
-    go build main.go
     ./main
-    # /eklhad/eklhadd start
     echo "Server started"
 else
     exec "$@"
