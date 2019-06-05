dcedc9dce548efbe20e21e4f1b8f15bf297b5082
Neil Dahlke <neil@dahlke.io>
2019-06-04 20:19:57 -0700

    Working CircleCI testing.

--- web/services/links_test.go
+++ web/services/links_test.go
@@ -1,14 +1,17 @@
 package services
 
 import (
+	"os"
 	"testing"
 )
 
 var SCPR_HACKDAY_ID int64 = 21
 
 func TestLinks(t *testing.T) {
+	os.Chdir("../")
 	links := GetLinks()
-	firstLink := links.PressLinks[0]
+	firstLink := links[0]
+
 	if firstLink.ID != SCPR_HACKDAY_ID {
 		t.Errorf("Link ID was incorrect, got: %d, want: %d.", firstLink.ID, SCPR_HACKDAY_ID)
 	}
--- web/services/locations_test.go
+++ web/services/locations_test.go
@@ -7,8 +7,8 @@ import (
 var SOUTH_BEND_ID int64 = 43
 
 func TestLocations(t *testing.T) {
-	locations := GetLocations()
-	firstLocationID := locations[0].ID
+	locations := GetLocationsGeoJSON().Features
+	firstLocationID := locations[0].Properties["id"]
 	if firstLocationID != SOUTH_BEND_ID {
 		t.Errorf("ID was incorrect, got: %d, want: %d.", firstLocationID, SOUTH_BEND_ID)
 	}
