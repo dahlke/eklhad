19459ce04fa72bac323e8031725bb126eea289a3
Neil Dahlke <neil@dahlke.io>
2019-09-18 18:21:21 -0700

    Fix the tests to pass the new data structure parsing.

--- web/services/locations_test.go
+++ web/services/locations_test.go
@@ -1,15 +1,17 @@
 package services
 
 import (
+	"fmt"
 	"testing"
 )
 
-var SOUTH_BEND_ID int64 = 43
+var SOUTH_BEND_ID float64 = 0
 
 func TestLocations(t *testing.T) {
 	locations := GetLocationsGeoJSON().Features
 	firstLocationID := locations[0].Properties["id"]
+	fmt.Println(locations[0].Properties["id"])
 	if firstLocationID != SOUTH_BEND_ID {
-		t.Errorf("ID was incorrect, got: %d, want: %d.", firstLocationID, SOUTH_BEND_ID)
+		t.Errorf("ID was incorrect, got: %f, want: %f.", firstLocationID, SOUTH_BEND_ID)
 	}
 }
