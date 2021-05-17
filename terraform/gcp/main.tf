2966b19b16e87bc21f6f9de79900395cd7cff93f
Neil Dahlke <neil.dahlke@gmail.com>
2021-05-16 19:05:11 -0700

    Adding the new hostname that will be used.

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -39,7 +39,7 @@ resource "acme_registration" "reg" {
 resource "acme_certificate" "certificate" {
   account_key_pem           = acme_registration.reg.account_key_pem
   common_name               = "dahlke.io"
-  subject_alternative_names = ["www.dahlke.io", "gcp.dahlke.io", "aws.dahlke.io", "foo.dahlke.io"]
+  subject_alternative_names = ["www.dahlke.io", "gcp.dahlke.io", "aws.dahlke.io", "static.dahlke.io"]
 
   dns_challenge {
     provider = "cloudflare"
