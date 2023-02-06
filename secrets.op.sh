e50daf6176b10305e9259b5a324ded58bb3adc74
Neil Dahlke <neil.dahlke@gmail.com>
2023-02-06 14:27:57 -0800

    Adding GH actions workflow

--- secrets.op.sh
+++ secrets.op.sh
@@ -7,11 +7,13 @@ eval $(op signin my)
 
 # For Packer and Terraform
 export GOOGLE_APPLICATION_CREDENTIALS="/Users/neildahlke/.gcp/eklhad-web-packer.json"
+# For GH Actions
+# export GOOGLE_APPLICATION_CREDENTIALS="/home/runner/work/eklhad/eklhad/gcp-actions.json"
 # For the data collector
 export GOOGLE_API_KEY=$(op get item "Google dahlke.io" | jq -r '.details.sections[1].fields[2].v')
 
-export AWS_ACCESS_KEY_ID=$(op get item "Amazon" | jq -r '.details.sections[1].fields[0].v')
-export AWS_SECRET_ACCESS_KEY=$(op get item "Amazon" | jq -r '.details.sections[1].fields[1].v')
+# export AWS_ACCESS_KEY_ID=$(op get item "Amazon" | jq -r '.details.sections[1].fields[0].v')
+# export AWS_SECRET_ACCESS_KEY=$(op get item "Amazon" | jq -r '.details.sections[1].fields[1].v')
 
 export TWITTER_CONSUMER_API_KEY=$(op get item Twitter | jq -r '.details.sections[1].fields[0].v')
 export TWITTER_CONSUMER_SECRET_KEY=$(op get item Twitter | jq -r '.details.sections[1].fields[1].v')
