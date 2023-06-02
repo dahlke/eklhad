2555577c7045471a536d21e88d4d10e7fe8c5a45
Neil Dahlke <neil.dahlke@gmail.com>
2023-06-01 21:31:24 -0700

    Updating OP logic and cert.

--- secrets.op.sh
+++ secrets.op.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-eval $(op signin my)
+eval $(op signin)
 
 # NOTE: I think I can remove this.
-# export GCLOUD_KEYFILE_JSON=$(op get item "Google dahlke.io" | jq -r '.details.sections[1].fields[0].v' | jq -r .)
+# export GCLOUD_KEYFILE_JSON=$(op item get "Google dahlke.io" --format=json | jq -r '.fields[3].value' --format=json | jq -r)
 
 # For Packer and Terraform
 export GOOGLE_APPLICATION_CREDENTIALS="/Users/neildahlke/.gcp/eklhad-web-packer.json"
 # For GH Actions
 # export GOOGLE_APPLICATION_CREDENTIALS="/home/runner/work/eklhad/eklhad/gcp-actions.json"
 # For the data collector
-export GOOGLE_API_KEY=$(op get item "Google dahlke.io" | jq -r '.details.sections[1].fields[2].v')
+export GOOGLE_API_KEY=$(op item get "Google dahlke.io" --format=json | jq -r '.fields[5].value')
 
-# export AWS_ACCESS_KEY_ID=$(op get item "Amazon" | jq -r '.details.sections[1].fields[0].v')
-# export AWS_SECRET_ACCESS_KEY=$(op get item "Amazon" | jq -r '.details.sections[1].fields[1].v')
+# export AWS_ACCESS_KEY_ID=$(op item get "Amazon" --format=json | jq -r '.fields[3].value')
+# export AWS_SECRET_ACCESS_KEY=$(op item get "Amazon" --format=json | jq -r '.fields[4].value')
 
-export TWITTER_CONSUMER_API_KEY=$(op get item Twitter | jq -r '.details.sections[1].fields[0].v')
-export TWITTER_CONSUMER_SECRET_KEY=$(op get item Twitter | jq -r '.details.sections[1].fields[1].v')
+export TWITTER_CONSUMER_API_KEY=$(op item get Twitter --format=json | jq -r '.fields[3].value')
+export TWITTER_CONSUMER_SECRET_KEY=$(op item get Twitter --format=json | jq -r '.fields[4].value')
 
-export GITHUB_TOKEN=$(op get item GitHub | jq -r '.details.sections[1].fields[0].v')
-export GITHUB_SECRET=$(op get item GitHub | jq -r '.details.sections[1].fields[1].v')
+export GITHUB_TOKEN=$(op item get GitHub --format=json | jq -r '.fields[3].value')
+export GITHUB_SECRET=$(op item get GitHub --format=json | jq -r '.fields[4].value')
 
-export CLOUDFLARE_TOKEN=$(op get item Cloudflare | jq -r '.details.sections[1].fields[0].v')
-export CLOUDFLARE_EMAIL=$(op get item Cloudflare | jq -r '.details.sections[1].fields[1].v')
-export CLOUDFLARE_API_KEY=$(op get item Cloudflare | jq -r '.details.sections[1].fields[2].v')
+export CLOUDFLARE_TOKEN=$(op item get Cloudflare --format=json | jq -r '.fields[3].value')
+export CLOUDFLARE_EMAIL=$(op item get Cloudflare --format=json | jq -r '.fields[4].value')
+export CLOUDFLARE_API_KEY=$(op item get Cloudflare --format=json | jq -r '.fields[5].value')
 
-export INSTAGRAM_ACCESS_TOKEN=$(op get item Instagram | jq -r '.details.sections[1].fields[0].v')
+export INSTAGRAM_ACCESS_TOKEN=$(op item get Instagram --format=json | jq -r '.fields[3].value')
 
-export TFC_TOKEN=$(op get item "Terraform Cloud" | jq -r '.details.sections[1].fields[0].v')
+export TFC_TOKEN=$(op item get "Terraform Cloud" --format=json | jq -r '.fields[3].value')
