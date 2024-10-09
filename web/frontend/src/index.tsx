9fe4eb0a4a656f7f72d99c50b0489c337ff2aed8
Neil Dahlke <neil.dahlke@gmail.com>
2024-10-08 19:06:53 -0700

    Last files done with the lazy migration to TS.

--- web/frontend/src/index.tsx
+++ web/frontend/src/index.tsx
@@ -9,8 +9,6 @@ import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
 import { fetchLocations } from "./actions/locations";
 import { fetchGravatar } from "./actions/gravatar";
-// import { fetchLinks } from "./actions/links";
-// import { fetchBlogs } from "./actions/blogs";
 import rootReducer from "./reducers/index";
 import PopulatedApp from "./container/PopulatedApp";
 
@@ -20,30 +18,24 @@ import * as serviceWorker from "./serviceWorker";
 
 const loggerMiddleware = createLogger();
 const store = createStore(
-	rootReducer,
-	applyMiddleware(
-		thunkMiddleware, // lets us dispatch() functions
-		loggerMiddleware // neat middleware that logs actions
-	),
+    rootReducer,
+    applyMiddleware(
+        thunkMiddleware, // lets us dispatch() functions
+        loggerMiddleware // neat middleware that logs actions
+    ),
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// store.dispatch(fetchLocations()).then(() => console.log(store.getState()))
-// store.dispatch(fetchLinks()).then(() => console.log(store.getState()))
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 store.dispatch(fetchLocations());
 store.dispatch(fetchGravatar());
 
-// store.dispatch(fetchLinks());
-// store.dispatch(fetchBlogs());
-
 root.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<PopulatedApp />
-		</Provider>
-	</React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <PopulatedApp />
+        </Provider>
+    </React.StrictMode>,
 );
 
 // If you want your app to work offline and load faster, you can change
