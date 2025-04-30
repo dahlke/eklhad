bc0881cd5be74fc0bfabc1d7c191e750f0402291
Neil Dahlke <neil.dahlke@gmail.com>
2025-04-30 13:52:22 -0700

    Update CloudFlare config.

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -34,7 +34,7 @@ resource "acme_registration" "reg" {
 resource "acme_certificate" "certificate" {
   account_key_pem           = acme_registration.reg.account_key_pem
   common_name               = "dahlke.io"
-  subject_alternative_names = ["gcp.dahlke.io", "gcp4.dahlke.io"]
+  subject_alternative_names = ["gcp.dahlke.io", "gcp7.dahlke.io"]
   # NOTE: Due to the expiration of DST Root CA X3, we use ISRG Root X1
   preferred_chain = "ISRG Root X1"
 
@@ -120,7 +120,7 @@ resource "null_resource" "setup-web" {
       "touch /home/ubuntu/go/src/github.com/dahlke/eklhad/web/acme_cert.pem",
       "touch /home/ubuntu/go/src/github.com/dahlke/eklhad/web/acme_issuer.pem",
       "touch /home/ubuntu/go/src/github.com/dahlke/eklhad/web/acme_private_key.pem",
-      "echo \"${acme_certificate.certificate.certificate_pem}${acme_certificate.certificate.issuer_pem}\" > /home/ubuntu/go/src/github.com/dahlke/eklhad/web/acme_cert.pem",
+      "echo -e \"${acme_certificate.certificate.certificate_pem}\n${acme_certificate.certificate.issuer_pem}\" > /home/ubuntu/go/src/github.com/dahlke/eklhad/web/acme_cert.pem",
       "echo \"${acme_certificate.certificate.private_key_pem}\" > /home/ubuntu/go/src/github.com/dahlke/eklhad/web/acme_private_key.pem",
       "cd /home/ubuntu/go/src/github.com/dahlke/eklhad/web/",
       "touch /tmp/eklhad-web-logs.txt",
@@ -130,12 +130,14 @@ resource "null_resource" "setup-web" {
   }
 }
 
+
 resource "cloudflare_record" "gcp" {
   zone_id = var.cloudflare_zone_id
   name    = "gcp"
   value   = google_compute_address.web.address
   type    = "A"
 }
+
 
 /*
 NOTE: These resources could also be managed from the AWS configuration in the
@@ -153,4 +155,38 @@ resource "cloudflare_record" "dahlkeio" {
   name    = "dahlke.io"
   value   = google_compute_address.web.address
   type    = "A"
+}
+
+# Add www subdomain as CNAME to root domain
+resource "cloudflare_record" "www" {
+  zone_id = var.cloudflare_zone_id
+  name    = "www"
+  value   = "dahlke.io"
+  type    = "CNAME"
+  proxied = true
+}
+
+# Add MX record for email
+resource "cloudflare_record" "mx" {
+  zone_id  = var.cloudflare_zone_id
+  name     = "dahlke.io"
+  value    = "mail.dahlke.io"
+  type     = "MX"
+  priority = 10
+}
+
+# Add SPF record to prevent email spoofing
+resource "cloudflare_record" "spf" {
+  zone_id = var.cloudflare_zone_id
+  name    = "dahlke.io"
+  value   = "v=spf1 -all"
+  type    = "TXT"
+}
+
+# Add DMARC record
+resource "cloudflare_record" "dmarc" {
+  zone_id = var.cloudflare_zone_id
+  name    = "_dmarc"
+  value   = "v=DMARC1; p=reject; rua=mailto:dmarc@dahlke.io"
+  type    = "TXT"
 }
