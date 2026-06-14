d708e7eb62779a5575f37a061388bf07c55f8feb
Neil Dahlke <neil.dahlke@gmail.com>
2026-06-14 09:51:22 -0700

    Updating secrets file, updating locations.

--- secrets.op.sh
+++ secrets.op.sh
@@ -5,11 +5,11 @@ eval $(op signin)
 # For Packer and Terraform
 export GOOGLE_APPLICATION_CREDENTIALS="/Users/neil/.gcp/eklhad-web-packer.json"
 # For the data collector
-export GOOGLE_API_KEY=$(op item get "Google dahlke.io" --format=json | jq -r '.fields[5].value')
+export GOOGLE_API_KEY=$(op item get "Google dahlke.io" --format=json | jq -r '.fields[] | select(.label == "GOOGLE_API_KEY") | .value')
 
-export CLOUDFLARE_TOKEN=$(op item get Cloudflare --format=json | jq -r '.fields[3].value')
-export CLOUDFLARE_EMAIL=$(op item get Cloudflare --format=json | jq -r '.fields[4].value')
-export CLOUDFLARE_API_KEY=$(op item get Cloudflare --format=json | jq -r '.fields[5].value')
+export CLOUDFLARE_TOKEN=$(op item get Cloudflare --format=json | jq -r '.fields[] | select(.label == "CLOUDFLARE_TOKEN") | .value')
+export CLOUDFLARE_EMAIL=$(op item get Cloudflare --format=json | jq -r '.fields[] | select(.label == "CLOUDFLARE_EMAIL") | .value')
+export CLOUDFLARE_API_KEY=$(op item get Cloudflare --format=json | jq -r '.fields[] | select(.label == "CLOUDFLARE_API_KEY") | .value')
 
 # Mapbox token — dev token is localhost-restricted; prod token is used in docker builds
 export VITE_MAPBOX_TOKEN=$(op item get "MapBox" --format=json | jq -r '.fields[] | select(.label == "Mapbox dahlke.io Dev") | .value')
